213d5b09c068e0763d64539f70773a2844268770
Sangsun Lee <sangsunlee.aiden@gmail.com>
2024-11-09 08:07:11 +0900

    ss changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
+from flask import Flask
+
+# Define a factory function to create the app
+def create_app():
+    app = Flask(__name__)
+
+    # Import and register blueprints or routes
+    from .route import app as route_app
+    app.register_blueprint(route_app)
+
+    return app
+
  
