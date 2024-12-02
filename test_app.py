a06552253ca31c35a0fb64f466164eec340607fe
krunalpatel355 <krunalpatel35538@gmail.com>
2024-12-01 20:58:05 -0500

    start file

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
-from app import app
+from app.app import app
 
 def test_home():
-    response = app.test_client().get("/")
-    assert response.status_code == 200
-    assert b"<title>My Portfolio</title>" in response.data  # Check for the title tag in the HTML
+    response=app.test_client().get("/")
+
+    assert response.status_code==200
+    assert response.data== " "
